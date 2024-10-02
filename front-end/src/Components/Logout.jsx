e03cd09c189a9680ca51040eb173c1acd4940a39
kartik5026 <87748398+kartik5026@users.noreply.github.com>
2024-10-02 12:32:16 +0530

    Update Logout.jsx

--- front-end/src/Components/Logout.jsx
+++ front-end/src/Components/Logout.jsx
@@ -1,7 +1,7 @@
 function Logout(){
     return(
         <>
-        <form action="https://cashappbackend.onrender.com/logout" method="post">
+        <form action="https://expense-tracker-app-cpuk.onrender.com/logout" method="post">
             <button className="bg-blue-700 px-4 py-2 rounded-xl text-white mx-4">Log Out</button>
         </form>
         </>
